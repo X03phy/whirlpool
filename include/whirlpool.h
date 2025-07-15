8b203965022d4983e8a0c2c83d9ed25387b8d552
Elio Bonutto <elio.bonutto@gmail.com>
2025-07-15 10:32:40 +0200

    modification de 32 en 64 dans la struct

--- include/whirlpool.h
+++ include/whirlpool.h
@@ -545,7 +545,7 @@ static const uint64_t C7[256] = {
 
 typedef struct s_whirlpool_ctx
 {
-	uint32_t state[8];  // H0 to H7
+	uint64_t state[8];  // H0 to H7
 	uint64_t bitlen;    // Total length in bits
 	uint8_t buffer[64]; // 512 bits (64 bytes) buffer
 	size_t buffer_len;  // Actual length of the buffer
