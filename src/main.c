79060f133dc223751130208a38202b4b0cca698d
Elio Bonutto <elio.bonutto@gmail.com>
2025-07-16 14:27:30 +0200

    norme

--- src/main.c
+++ src/main.c
@@ -20,14 +20,14 @@ int main( int argc, char **argv )
 	WHIRLPOOL( ( const unsigned char * ) argv[1], strlen( argv[1] ), hash );
 	printf( "openssl WHIRLPOOL: " );
 	for ( uint8_t i = 0; i < 64; ++i )
-		printf("%02x", hash[i]);
+		printf( "%02x", hash[i] );
 	printf( "\n" );
 
-	whirlpool( ( const unsigned char * ) argv[1], strlen(argv[1]), my_hash );
+	whirlpool( ( const unsigned char * ) argv[1], strlen( argv[1] ), my_hash );
 	printf( "my WHIRLPOOL:      " );
 	for ( uint8_t i = 0; i < 64; ++i )
-		printf("%02x", my_hash[i]);
-	printf("\n");
+		printf( "%02x", my_hash[i] );
+	printf( "\n" );
 
 	if ( !memcmp( ( const char * ) hash, ( const char * ) my_hash, 64 ) )
 		printf( "OK\n" );
